cd782b3983e71c0424d94ea685b5efe61a508aa7
Abdalrhman yasser arfat <abdoarfat2006@gmail.com>
2025-02-23 01:56:08 +0200

    Update GameCard.tsx

--- src/components/comone/GameCard.tsx
+++ src/components/comone/GameCard.tsx
@@ -28,7 +28,7 @@ const GameCard = ({ item }: { item: Game }) => {
           <Link
             to={item.game_url}
             target="_blank"
-            className="absoulte bottom-1  text-white game-link-btn focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-1 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="absoulte bottom-1  text-white game-link-btn focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Vist Game
           </Link>
